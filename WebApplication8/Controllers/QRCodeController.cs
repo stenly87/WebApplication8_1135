0a88eacb001560be8b15d4fe12b48fcc8d8946e3
ppk <ppk_student@hotmail.com>
2021-11-12 11:35:21 +1000

    full rebuild

---
+++ WebApplication8/Controllers/QRCodeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QRCodeController : ControllerBase
+    {
+        static Dictionary<int, QRCode> codes = new Dictionary<int, QRCode>();
+        static int autoincrement = 1;
+
+        [HttpGet]
+        public IEnumerable<QRCode> Get()
+        {
+            return codes.Values;
+        }
+
+        [HttpPost]
+        public QRCode Post(QRCode code)
+        {
+            code.ID = autoincrement++;
+            codes.Add(code.ID, code);
+            return code;
+        }
+
+        [HttpPut]
+        public bool Put(QRCode code)
+        {
+            if (codes.ContainsKey(code.ID))
+            {
+                codes[code.ID] = code;
+                return true;
+            }
+            return false;
+        }
+
+        [HttpDelete]
+        public bool Delete(QRCode code)
+        {
+            if (codes.ContainsKey(code.ID))
+            {
+                codes.Remove(code.ID);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ WebApplication8/Models/QRCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class QRCode
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public long DateEnd { get; private set; }
+    }
+}
